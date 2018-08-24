24b19e0e126518ad35599547c97cf66b446ff731
sergey <serger777@gmail.com>
2018-08-24 23:30:05 +0300

    ДЗ-2

--- dz_2/index.php
+++ dz_2/index.php
@@ -20,4 +20,22 @@ echo "<h3>задача 2</h3> ";
 
 task2('*', 1, 2, 3, 5.2);
 
+
+echo "<h3>задача 3</h3> ";
+
+task3(4, 2);
+
+echo "<h3>задача 4</h3> ";
+
+
+task4();
+
+echo "<h3>задача 5</h3> ";
+
+task5();
+
+echo "<h3>задача 6</h3> ";
+//build_fail();
+task6("test3.txt", "Hello again2");
+
 ?>
--- dz_2/src/function.php
+++ dz_2/src/function.php
@@ -9,8 +9,6 @@ function task1($arr, $bool = false)
         $str = implode(".", $arr);
         return $str;
     }
-
-
 }
 
 
@@ -20,8 +18,104 @@ function task2($operator, $item)
     print_r($arr);
     print_r("<p>  выполняем соединяем массив вырезая $operator </p> ");
     print_r(eval('return ' . implode($operator, array_slice($arr, 1)) . ';'));
+}
+
+function task3($a, $b)
+{
+    print_r("<p>вы ввели  $a и   $b   </p>");
+    $z1 = is_int($a);
+    $z2 = is_int($b);
+
+    if ($z1 > 0 and $z2 > 0) {
+        echo('таблица умножения' . '</br>');
+        echo('<table>');
+        for ($x = 1; $x <= $a; $x++) {
+            echo '<tr>';
+            for ($y = 1; $y <= $b; $y++) {
+                $s = $x * $y;
+                echo '<td style="text-align:center;">';
+                if (($x % 2 == 0) && ($y % 2 == 0)) {
+                    echo '(' . $s . ')';
+                } elseif (($x % 2 == 1) && ($y % 2 == 1)) {
+                    echo '[' . $s . ']';
+                } else {
+                    echo ' ' . $s . ' ';
+                }
+                echo '</td>';
+
+            }
+            echo '</tr>';
+
+        }
+
+
+        echo('</table>');
+    } else {
+        echo "числа не целые";
+    }
+}
+
+function task4()
+{
+    $today = date("d-m-y H:i:s");
+    echo $today . "</br>";
+    echo date("d-m-y H:i:s", mktime(0, 0, 0, 02, 24, 2016));
+}
+
+function task5()
+{
+    $string = 'Карл у Клары украл Кораллы';
+    $upcase = '/К/u';
+    $result = preg_replace($upcase, '', $string);
+    echo $result;
+    echo "<br>";
+    $string2 = 'Две бутылки лимонада';
+    $upcase2 = '/Две/u';
+    $result = preg_replace($upcase2, 'Три', $string2);
+    echo $result;
+    echo "<br>";
+
 
 }
 
+function build_fail()
+{
+    $build = fopen('test.txt', 'w');
+    if ($build) {
+        echo 'файл test.txt создан';
+    }
+    $fw = fopen("test.txt", "a");
+    $mytext = "Hello again"; // Исходная строка
+    $test = fwrite($fw, $mytext); // Запись в файл
+    if ($test) {
+        echo 'Данные в файл успешно занесены.';
+    } else {
+        echo 'Ошибка при записи в файл.';
+    }
+    fclose($fw); //Закрытие файла
+}
+
+function task6($file_name, $fail_text)
+{
+    $build = fopen("$file_name", 'w');
+    if ($build) {
+        print_r("файл $file_name создан");
+    }
+    $fw = fopen("$file_name", "a");
+    $mytext = "$fail_text"; // Исходная строка
+    $test = fwrite($fw, $mytext); // Запись в файл
+    if ($test) {
+        echo 'Данные в файл успешно занесены.';
+    } else {
+        echo 'Ошибка при записи в файл.';
+    }
+
+    fclose($fw); //Закрытие файла
+
+    $readf = fopen("$file_name", "r");
+
+    echo "</br> Файл содержит: " . fread($readf, 1024) . "</br";
+
+}
 
 ?>
