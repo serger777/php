86cec5d15698de3d4d10c43af7d9e41cb44f5ad6
sergey <serger777@gmail.com>
2018-09-11 10:24:12 +0300

    dz-5

--- dz_5/task_3.php
+++ dz_5/task_3.php
@@ -1,5 +1,5 @@
-<?php
-require "../vendor/autoload.php";
+<?require_once "../vendor/autoload.php";
+use Intervention\Image\ImageManagerStatic as Image;
 // open an image file
 $img = Image::make('img/cotel_3.png');
 
@@ -8,5 +8,29 @@ $img = Image::make('img/cotel_3.png')->resize(320, 240)->insert('img/icon.png');
 
 // finally we save the image as a new file
 $img->save('img/bar.jpg');
+$img->rotate(-45);
+$img->save('img/bar2.jpg');
+// create empty canvas with background color
+$img2 = Image::canvas(320, 240, '#ddd');
+
+// define polygon points
+$points = array(
+    40,  50,  // Point 1 (x, y)
+    20,  240, // Point 2 (x, y)
+    60,  60,  // Point 3 (x, y)
+    240, 20,  // Point 4 (x, y)
+    50,  40,  // Point 5 (x, y)
+    10,  10   // Point 6 (x, y)
+);
+
+// draw a filled blue polygon with red border
+$img2->polygon($points, function ($draw) {
+    $draw->background('#0000ff');
+    $draw->border(2, '#ff0000');
+
+});
+$img2->save('img/bar3.jpg');
 ?>
 <img src="img/bar.jpg" >
+<img src="img/bar2.jpg" >
+<img src="img/bar3.jpg" >
